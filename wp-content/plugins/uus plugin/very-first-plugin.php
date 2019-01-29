a3ec553794cc0517a8cdb273d65ced92200e8dc0
merlepakkoiktkhk <merle.pakko@khk.ee>
2019-01-29 11:31:20 +0200

    modified plugin `Very first plugin` (after activation of `Very first plugin`)

---
+++ wp-content/plugins/uus plugin/very-first-plugin.php
@@ -0,0 +1,8 @@
+<?php 
+/** 
+ * Plugin name: Very first plugin
+ * plugin url: http:/merlepakko.ikt.khk.ee/
+ * description: this is the very first plugin i ever created
+ * version 1.0
+ * author: merle päkko
+ * */
