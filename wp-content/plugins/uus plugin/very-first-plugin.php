9f2a4aab1922b226debf919e89fe81a3b2f51417
merlepakkoiktkhk <merle.pakko@khk.ee>
2019-01-30 13:37:23 +0200

    deleted plugin `very-first-plugin.php`

--- wp-content/plugins/uus plugin/very-first-plugin.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/** 
- * Plugin name: Very first plugin
- * plugin url: http:/merlepakko.ikt.khk.ee/
- * description: this is the very first plugin i ever created
- * version 1.0
- * author: merle päkko
- * */
